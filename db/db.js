ce63d436d243f36214ee7513ab404c17c0138c9f
SOR7O <angelsorto330@gmail.com>
2024-01-05 05:38:25 -0600

    Add start to package json to init node

--- db/db.js
+++ db/db.js
@@ -1,8 +1,11 @@
 const mongoose= require("mongoose")
 require('dotenv').config();
 
-mongoose.connect(process.env.db_connection).then(() => {
+mongoose.connect(process.env.db_connection, {useNewUrlParser: true,
+   useUnifiedTopology: true}).then(() => {
     console.log("Database Connected");
-  });
+  }).then((connec)=>{
+    console.log(connec);
+  }).catch((err)=>{console.log(err);});
 
   module.exports= {mongoose};
